8a6c2a19c79ac54de3ec8127142a1cdab783279e
ManiCoding <ManiCoding@outlook.com>
2022-04-07 09:07:13 +0200

    Add project files.

---
+++ ListFromFolderApp/FileReader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ListFromFolder
+{
+    internal class FileReader
+    {   
+        // Folder Path
+        public string Path { get; set; }
+        // rawFiles = DirectoryInfo
+        public FileInfo[] rawFiles { get; set; }
+
+        public FileReader(string path)
+        {
+            Path = path;
+            Readfiles(Path); 
+        }
+
+        // Read DirectoryInfo to rawFiles
+        public void Readfiles(string path) 
+        {   
+            DirectoryInfo diSearch = new DirectoryInfo(Path);
+            rawFiles = diSearch.GetFiles();
+
+        }
+
+        // Write the File-Names to a Txt-File
+        public void SaveToFile(string path)
+        {   
+
+            FileStream stream = new FileStream(path, FileMode.Create, FileAccess.Write);
+            StreamWriter _writer = new StreamWriter(stream, Encoding.UTF8);
+
+            foreach(FileInfo file in rawFiles)
+                _writer.WriteLine(file.Name);
+
+            Console.WriteLine($"\n_List.txt from {Path} created at {DateTime.Now}.");
+
+            // Write instant and close
+            _writer.Flush();
+            _writer.Close();
+            stream.Close();
+
+        }
+
+        // Print the Files to Console
+        public void Print()
+        {
+            Console.WriteLine("\n ***** Files ***** \n");
+            foreach(FileInfo file in rawFiles)
+                Console.WriteLine(file.Name);
+        }
+    }
+}
---
+++ ListFromFolderApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+/* by mgeissle
+ * V1 File Name from a Folder to a List
+ * 
+ * V2 Ideas:
+ * User can choose with or without Extension
+ * User can choose txt or csv file
+ * Folder and Sub-Folder in one list
+ * 
+ *
+ */
+
+namespace ListFromFolder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("***********************************************************\n" +
+                                  "************** File-Name from Folder to List v1************\n" +
+                                  "***********************************************************\n");
+                Console.Write("Insert Path (with copy and paste): ");
+                string dirPath = Console.ReadLine();
+                FileReader fileReader = new FileReader(dirPath);
+                fileReader.Print();
+                string newpath = Path.Combine(dirPath, "_List.txt");
+                fileReader.SaveToFile(newpath);
+                Console.WriteLine();
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+        }
+    }
+}
